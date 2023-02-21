3b681dbea2a6c24bd7668fa67ba9a350954c128d
Esbree <noelva@gmx.de>
2023-02-21 17:59:54 +0100

    size limit to 100

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let board = document.querySelector('.board');
+let setSizeHeading = document.querySelector('h2');
 
 function changeColor() {
   let pixels = document.querySelectorAll('.board > div');
@@ -17,14 +18,21 @@ function fillBoard() {
   clearBoard();
   let inputField = document.querySelector('input').value;
   let size = inputField;
-  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  for (i = 0; i < size * size; i++) {
-    let pixel = document.createElement('div');
-    pixel.style.backgroundColor = 'lightgrey';
-    board.insertAdjacentElement('beforeend', pixel);
+  if (size > 100) {
+    setSizeHeading.textContent = '!!!choose a lower size!!!';
+  } else {
+    setSizeHeading.textContent = 'set size below';
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (i = 0; i < size * size; i++) {
+      let pixel = document.createElement('div');
+      pixel.style.backgroundColor = 'lightgrey';
+      board.insertAdjacentElement('beforeend', pixel);
+    }
+
+    changeColor();
   }
-
-  changeColor();
 }
+
