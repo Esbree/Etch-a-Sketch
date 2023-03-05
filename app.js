904eac0045f5ec5bdebbceb4f392d548fe630fe3
Esbree <noelva@gmx.de>
2023-03-05 19:09:06 +0100

    formatting

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
-let board = document.querySelector('.board');
-let setSizeHeading = document.querySelector('h3');
+let board = document.querySelector(".board");
+let setSizeHeading = document.querySelector("h3");
 
 function changeColor() {
-  const pixels = document.querySelectorAll('.board > div');
+  const pixels = document.querySelectorAll(".board > div");
   pixels.forEach((pixel) => {
-    pixel.addEventListener('mouseover', () => {
-      pixel.style.backgroundColor = 'black';
-    })
-  })
+    pixel.addEventListener("mouseover", () => {
+      pixel.style.backgroundColor = "black";
+    });
+  });
 }
 
 function clearBoard() {
-  board.innerHTML = '';
+  board.innerHTML = "";
 }
 
 function fillBoard() {
   clearBoard();
-  let inputField = document.querySelector('input').value;
+  let inputField = document.querySelector("input").value;
   let size = inputField;
 
   if (size > 100) {
-    setSizeHeading.textContent = '!!!choose a lower size!!!';
+    setSizeHeading.textContent = "!!!choose a lower size!!!";
   } else {
-    setSizeHeading.textContent = 'set size below';
+    setSizeHeading.textContent = "set size below";
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for (i = 0; i < size * size; i++) {
-      let pixel = document.createElement('div');
-      pixel.style.backgroundColor = 'lightgrey';
-      board.insertAdjacentElement('beforeend', pixel);
+      let pixel = document.createElement("div");
+      pixel.style.backgroundColor = "lightgrey";
+      board.insertAdjacentElement("beforeend", pixel);
     }
 
     changeColor();
   }
 }
-
